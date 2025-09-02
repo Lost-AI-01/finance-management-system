2079cbe1daa1484dd1f6df6f6762bbeffa47f530
Lost-AI-01 <lost615793@gmail.com>
2025-09-02 21:30:11 +0530

    adding css for files

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" bg-dark text-light p-4">
+    <div className="footer">
       <h6 className="text-center text-white">
         All rights reserved &copy; Shubham
       </h6>
